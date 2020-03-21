4c281686fa658bf5d72d2834d5d7be70872f084c
Volodymyr <mlyzan@ukr.net>
2020-03-21 15:00:46 +0200

    modify sitter page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SitterContainer } from './sitter/sitter.container';
+import { SitterComponent } from './sitter/sitter.component';
 
 const routes: Routes = [
-  {path: 'sitter', component: SitterContainer}
+  {
+    path: 'sitter', component: SitterContainer,
+    children: [{path: '', component: SitterComponent}]
+  }
 ];
 
 @NgModule({
--- src/app/sitter/sitter.service.ts
+++ src/app/sitter/sitter.service.ts
@@ -6,10 +6,12 @@ export interface Sitter {
     email: string,
     photo: string,
     years: string,
-    pets: string,
+    services: string[],
+    animals: string[],
+    availability: string,
     payment: number,
-    location: string,
-    description: string
+    address: string,
+    information: string
 }
 
 @Injectable({
@@ -24,9 +26,11 @@ export class SitterService {
         years: '25',
         photo: 'https://www.petsit.com/stuff/contentmgr/files/0/884b1fb69d6413f97b81a120eee3402e/image/hp_petsitterresources_2018_as_man.jpg',
         payment: 20,
-        pets: 'Dogs, Cats',
-        location: 'Lviv',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem magni ipsum eum laboriosam rem porro quas eius expedita saepe? Ipsum inventore quis quibusdam ratione laudantium ipsam iure nobis officia suscipit error, qui cupiditate culpa exercitationem, dolor rem, nisi voluptatibus mollitia.'
+        services: [' Dog walking', ' Pet Boarding'],
+        animals: [' Dogs', ' Cats'],
+        availability: 'from 5 to 7',
+        address: 'Lviv',
+        information: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem magni ipsum eum laboriosam rem porro quas eius expedita saepe? Ipsum inventore quis quibusdam ratione laudantium ipsam iure nobis officia suscipit error, qui cupiditate culpa exercitationem, dolor rem, nisi voluptatibus mollitia.'
     };
 
     getSitter(): Sitter {
