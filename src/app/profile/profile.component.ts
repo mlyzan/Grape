05e7ffb522a7c83167478d46e4d68cbf3704a940
Volodymyr <mlyzan@ukr.net>
2020-04-11 15:49:10 +0300

    Added possibility to delete order

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Store, select } from '@ngrx/store';
 import { getUserInfo } from '../root-state/user/user.selectors';
-import { loadBooks } from '../root-state/sitter/sitter.actions';
+import { loadBooks, declineBook, loadSitters } from '../root-state/sitter/sitter.actions';
 import { getBooksByCustomerId } from '../root-state/sitter/sitter.selectors';
 import { getActiveId } from '../root-state/user/user.selectors';
 import { Book } from '../root-state/sitter/sitter.interfaces';
@@ -13,11 +13,12 @@ import { Book } from '../root-state/sitter/sitter.interfaces';
 })
 export class ProfileComponent implements OnInit {
   user: any;
-  responses: Book[];
+  responses: Book[] = [];
   activeId: string;
   constructor(private _store: Store) { }
 
   ngOnInit(): void {
+    this._store.dispatch(loadSitters());
     this._store.pipe(
       select(getActiveId)
     ).subscribe(res => this.activeId = res);
@@ -25,9 +26,20 @@ export class ProfileComponent implements OnInit {
       select(getUserInfo)
     ).subscribe(res => this.user = res);
     this._store.dispatch(loadBooks());
-    this._store.pipe(
-      select(getBooksByCustomerId(this.activeId))
-    ).subscribe(res => this.responses = [...res])
+    setTimeout(() => {
+      this._store.pipe(
+        select(getBooksByCustomerId(this.activeId))
+      ).subscribe(res => this.responses = [...res]);
+    }, 1000)
   }
 
+  onDecline(id: string): void {
+    this._store.dispatch(declineBook(id));
+    setTimeout(() => {
+      this._store.dispatch(loadBooks());
+      this._store.pipe(
+        select(getBooksByCustomerId(this.activeId))
+      ).subscribe(res => this.responses = [...res]);
+    }, 1000)
+  }
 }
