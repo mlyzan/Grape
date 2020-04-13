a7f96750a36ba8c4916411b504240ae6529f6005
Olena Starchak <58229959+olenastarchak@users.noreply.github.com>
2020-04-13 14:08:46 +0300

    edited css for navigation, hero section

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -10,6 +10,16 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  @HostListener('window:scroll', ['$event']) onWindowScroll(e) {
+    let element = document.querySelector('.header');
+    let element2 = document.querySelector('.nav');
+    if (window.pageYOffset > element2.clientHeight + 250) {
+      element.classList.add('faded');
+    } else {
+      element.classList.remove('faded');
+    }
   }
 
 }
